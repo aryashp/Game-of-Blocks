fd78452ade269ddf8caba1e4a255d53f47440c99
Aryash Pateriya <60755356+aryashp@users.noreply.github.com>
2021-05-25 01:15:23 +0530

    Create Ass1.py

---
+++ Assignment1/Ass1.py
@@ -0,0 +1,18 @@
+import hashlib
+import time
+ 
+string1 = input("Enter the string: ")
+ogstring = string1
+flag = 0
+i = 1
+start_time = time.time()
+while flag==0:
+  bytes = ogstring.encode(encoding='UTF-8',errors='strict') # read entire file as bytes
+  readable_hash = hashlib.sha256(bytes).hexdigest();
+  if readable_hash[0]=='0' and readable_hash[1]=='0' and readable_hash[2]=='0' and readable_hash[3]=='0' and readable_hash[4]=='0':
+    print("The hash value is =",readable_hash)  
+    print("The nonce Value is =",i-1)
+    flag=1
+  ogstring = string1 + str(i)
+  i+=1  
+print("Time taken: %s seconds" % (time.time() - start_time))
